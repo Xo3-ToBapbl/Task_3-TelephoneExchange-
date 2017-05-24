c4f73af48bc01072fb35878bb849824d59bf12a7
Jhon <jhonstupak@gmail.com>
2017-05-24 23:04:22 +0300

    Stats0.1

---
+++ ATE/BaseClasses/Statistic.cs
@@ -0,0 +1,24 @@
+﻿using ATE.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATE.BaseClasses
+{
+    public class Statistic
+    {
+        public int Number { get; }
+
+        public CallStats CallState { get; }
+
+        public int TargetNumber { get; }
+
+        public DateTime Start { get; }
+
+        public TimeSpan Span { get; }
+
+        public int Cost { get; }
+    }
+}
---
+++ ATE/Enums/CallStats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATE.Enums
+{
+    public enum CallStats
+    {
+        OutgoingCall,
+        IncomingCall,
+        NotCalled,
+        MissedCall,
+    }
+}
